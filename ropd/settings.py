605cdc70dbca84a95101a03d1326481d282ee5ba
Cleyton Fábio <cleyton.flb@gmail.com>
2020-10-21 01:22:42 -0300

    comentando static_root

--- ropd/settings.py
+++ ropd/settings.py
@@ -192,7 +192,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
 
 # TODO: Verificar se realmente necessita desta variavel
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
