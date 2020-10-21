aeef14fb157375f070508715c67a6886bb160868
Cleyton Fábio <cleyton.flb@gmail.com>
2020-10-20 22:12:47 -0300

    UPDATE: All hosts

--- ropd/settings.py
+++ ropd/settings.py
@@ -7,7 +7,7 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
