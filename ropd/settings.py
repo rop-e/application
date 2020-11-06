b3ad0f3fb45bd6c6ccc7f99ddf2d20069bed9ae3
Cleyton Fábio <cleyton.flb@gmail.com>
2020-11-06 13:27:53 -0300

    BUG: depois refatorar o sistema de arquivos staticos. Isso esta feio

--- ropd/settings.py
+++ ropd/settings.py
@@ -194,7 +194,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
 
 # TODO: Verificar se realmente necessita desta variavel
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
