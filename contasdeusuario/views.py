f403f97a32acc4b02ff01487aab9d0ac12312dc1
Gabriel Lobo <gabrielrblgbi@gmail.com>
2021-01-05 00:08:04 -0300

    correção de view

--- contasdeusuario/views.py
+++ contasdeusuario/views.py
@@ -4,23 +4,27 @@ from rest_framework.response import Response
 from contasdeusuario.serializers import AlterarSenhaSerializer
 from contasdeusuario.models import Usuario
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import (
+    SessionAuthentication,
+    TokenAuthentication
+)
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+
 
 class CustomAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
+                                           context={"request": request})
         serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
+        user = serializer.validated_data["user"]
+        token = Token.objects.get_or_create(user=user)
         return Response({
-            'token': token.key,
-            'user_id': user.pk,
-            'matricula': user.matricula,
-            'nome': user.nome,
-            'email': user.email
+            "token": token.key,
+            "user_id": user.pk,
+            "matricula": user.matricula,
+            "nome": user.nome,
+            "email": user.email
         })
 
 
@@ -30,7 +34,8 @@ class AlteracaoSenhaView(generics.UpdateAPIView):
     """
     serializer_class = AlterarSenhaSerializer
     model = Usuario
-    permission_classes = (IsAuthenticated,)
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get_object(self, queryset=None):
         obj = self.request.user
@@ -41,17 +46,16 @@ class AlteracaoSenhaView(generics.UpdateAPIView):
         serializer = self.get_serializer(data=request.data)
 
         if serializer.is_valid():
-            # Check old password
             if not self.object.check_password(serializer.data.get("old_password")):
-                return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
-            # set_password also hashes the password that the user will get
+                return Response({"old_password": ["Senha errada."]}, status=status.HTTP_400_BAD_REQUEST)
             self.object.set_password(serializer.data.get("new_password"))
             self.object.save()
+
             response = {
-                'status': 'success',
-                'code': status.HTTP_200_OK,
-                'message': 'Senha atualizada com sucesso!',
-                'data': []
+                "status": "success",
+                "code": status.HTTP_200_OK,
+                "message": "Senha atualizada com sucesso!",
+                "data": []
             }
 
             return Response(response)
