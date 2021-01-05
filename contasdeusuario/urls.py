ac84956555887b3c3005e9de024dba963ae957a5
Gabriel Lobo <gabrielrblgbi@gmail.com>
2021-01-04 22:05:35 -0300

    reset e alteração de senha url

---
+++ contasdeusuario/urls.py
@@ -0,0 +1,10 @@
+from .views import AlteracaoSenhaView
+from django.urls import (
+    path,
+    include
+)
+
+urlpatterns = [
+    path('alterar-senha/', AlteracaoSenhaView.as_view(), name='alterar-senha'),
+    path('resetar-senha/', include('django_rest_passwordreset.urls', namespace='resetar-senha'))
+]
