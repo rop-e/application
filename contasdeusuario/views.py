5047f93c29b709e58f25c3e0632def59e391fdd8
Gabriel Lobo <gabrielrblgbi@gmail.com>
2021-01-05 00:32:22 -0300

    configuração class customauthtoken corrigida

--- contasdeusuario/views.py
+++ contasdeusuario/views.py
@@ -11,14 +11,13 @@ from rest_framework.authentication import (
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
-
 class CustomAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data,
                                            context={"request": request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data["user"]
-        token = Token.objects.get_or_create(user=user)
+        token, created = Token.objects.get_or_create(user=user)
         return Response({
             "token": token.key,
             "user_id": user.pk,
