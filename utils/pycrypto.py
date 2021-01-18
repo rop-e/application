2d705d4af9df8a96627ea9e520518f32aa7cfe16
Gabriel Lobo <gabrielrblgbi@gmail.com>
2021-01-18 14:13:41 -0300

    função de encrypt e desencrypt

--- utils/pycrypto.py
+++ utils/pycrypto.py
@@ -1,35 +1,30 @@
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 from Crypto import Random
-import base64
+from Crypto.Util.Padding import pad, unpad
+from base64 import b64encode, b64decode
+from django.conf import settings
+
+password = str(settings.SENHA_HASH).encode()
 
 
-def encrypt(key, source, encode=True):
-    key = SHA256.new(str(key).encode()).digest()
-    IV = Random.new().read(AES.block_size)
+def encrypt(source):
+    key = SHA256.new(password).digest()
     source = str(source).encode()
+    IV = Random.new().read(AES.block_size)
 
     encryptor = AES.new(key, AES.MODE_CBC, IV)
-    padding = AES.block_size - len(source) % AES.block_size
-    source += bytes([padding]) * padding
+    result = encryptor.encrypt(pad(source, AES.block_size))
 
-    data = IV + encryptor.encrypt(source)
-
-    return base64.b64encode(data).decode() if encode else data
+    return b64encode(IV + result).decode()
 
 
-def decrypt(key, source, decode=True):
-    if decode:
-        source = base64.b64decode(source.encode())
-
-    key = SHA256.new(str(key).encode()).digest()
+def decrypt(source):
+    key = SHA256.new(password).digest()
+    source = b64decode(source.encode())
     IV = source[:AES.block_size]
 
     decryptor = AES.new(key, AES.MODE_CBC, IV)
-    data = decryptor.decrypt(source[AES.block_size:])
-    padding = data[-1]
+    result = unpad(decryptor.decrypt(source[AES.block_size:]), AES.block_size)
 
-    if data[-padding:] != bytes([padding]) * padding:
-        raise ValueError("Invalid padding...")
-
-    return bytes(data[:-padding]).decode("utf-8")
+    return bytes(result).decode()
