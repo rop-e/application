b5c6ed4a63b3e1271dbb92ea538d045ae167bd4f
cflb <cleyton.flb@gmail.com>
2021-02-24 12:26:19 +0000

    FIX: corrigindo erro caractere path ..media/

--- ropd/settings.py
+++ ropd/settings.py
@@ -210,7 +210,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 PDF_ROOT = MEDIA_ROOT + '/pdf/'  # Path pdf url
 
 ENVIRONMENT = os.environ.get('ENVIRONMENT')
